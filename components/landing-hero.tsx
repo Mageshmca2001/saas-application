fad705a3ed5a7fb79851dc370e4000ab4c47c469
Magesh.B <156915756+Mageshmca2001@users.noreply.github.com>
2024-09-13 19:02:13 +0530

    Update landing-hero.tsx

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -12,15 +12,12 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-6 font-extrabold">
-        <h1>The Best AI Tool for</h1>
+        <h1>The Better AI Tool for</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-orange-400">
           <TypewriterComponent
             options={{
               strings: [
                 "Chatbot.",
-                "Photo Generation.",
-                "Music Generation.",
-                "Video Generation.",
                 "Code Generation.",
               ],
               autoStart: true,
